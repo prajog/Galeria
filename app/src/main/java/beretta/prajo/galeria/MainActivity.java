9f0332922a082c208512706cef488d9c1124d792
prajog <gabrielapberetta@gmail.com>
2024-06-04 11:08:14 -0300

    APP Galeria - parei no passo 5

--- app/src/main/java/beretta/prajo/galeria/MainActivity.java
+++ app/src/main/java/beretta/prajo/galeria/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -15,6 +16,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        //obtem o elemento tbMain
+        Toolbar toolbar = findViewById(R.id.tbMain);
+
+        //indica que tbMain deve ser considerado como a ActionBar padrao da tela
+        setSupportActionBar(toolbar);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
